ac04459a55acfc2cca637d8aaf67e292d302656a
titosco <titobiloluwatella@gmail.com>
2025-07-07 06:51:29 +0100

    starting

---
+++ naobs/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">Welcome to NAOBS</h1>
+      <p className="mt-4 text-lg">
+        This is a simple Next.js application using Tailwind CSS.
+      </p>
+      </div>
+  );
+}
