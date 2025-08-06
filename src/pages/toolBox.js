9f91baee96d31636a3ba46c2cbcaee1480a23d20
titosco <titobiloluwatella@gmail.com>
2025-08-06 23:37:12 +0100

    final

---
+++ src/pages/toolBox.js
@@ -0,0 +1,13 @@
+import Navigation from "./_components/Navigation";
+import Footer from "./_components/Footer";
+import SectionTool from "./Section/ToolBox/SectionTool";
+
+export default function Event() {
+  return (
+    <div>
+      <Navigation />
+      <SectionTool />
+      <Footer />
+    </div>
+  );
+}
