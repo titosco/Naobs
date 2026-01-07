f13f7752a010b5067bc53023c77419f0b7196500
titosco <titobiloluwatella@gmail.com>
2026-01-07 17:24:32 +0100

    excos

--- src/pages/Section/Home/Section4.jsx
+++ src/pages/Section/Home/Section4.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Img1 from "../../../images/sample.jpeg";
-import Img2 from "../../../images/ugonma.jpg";
-import Img3 from "../../../images/Khalid.png";
-import Img4 from "../../../images/Jumoke.png";
-import Img5 from "../../../images/TITO.png";
-import Img6 from "../../../images/Utty.jpg";
-import Img7 from "../../../images/Sooft.jpg";
-import Img8 from "../../../images/Esther.png";
-import Img9 from "../../../images/Fash.png";
+import Img1 from "../../../images/kehinde.jpeg";
+import Img2 from "../../../images/ugonma.jpeg";
+import Img3 from "../../../images/jumoke.jpeg";
+import Img4 from "../../../images/peace.jpeg";
+import Img5 from "../../../images/olapade.jpeg";
+import Img6 from "../../../images/adedeji.jpeg";
+import Img7 from "../../../images/amade.jpeg";
+import Img8 from "../../../images/kachi.jpeg";
+import Img9 from "../../../images/daramora.jpeg";
+import Img10 from "../../../images/joy.jpeg";
 import Image from "next/image";
 
 const Section4 = () => {
@@ -24,15 +25,15 @@ const Section4 = () => {
         </span>
       </div>
       {/* cards */}
-      <div className="mt-4 grid relative md:grid-cols-2 md:gap-20 lg:grid-cols-3 lg:gap-6">
-        {/* card for 9 listings */}
+      <div className="mt-4 grid relative md:grid-cols-3 md:gap-20 lg:grid-cols-4 lg:gap-6">
+        {/* card for 10 listings */}
         {/* card1 */}
         <div className="card">
           <div className="card-inner">
             <div className="card-face">
               <Image alt="" src={Img1} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Kadiri Kehinde</span>
+                <span className="card-title">KADIRI KEHINDE</span>
                 <span className="card-desc">PRESIDENT</span>
               </div>
             </div>
@@ -44,7 +45,7 @@ const Section4 = () => {
             <div className="card-face">
               <Image alt="" src={Img2} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Akanwa Ugonma</span>
+                <span className="card-title">AKANWA UGONMA</span>
                 <span className="card-desc">VICE PRESIDENT</span>
               </div>
             </div>
@@ -56,7 +57,7 @@ const Section4 = () => {
             <div className="card-face">
               <Image alt="" src={Img3} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Khalid Duale</span>
+                <span className="card-title">AKANMU OLAJUMOKE</span>
                 <span className="card-desc">GENERAL SECRETARY</span>
               </div>
             </div>
@@ -68,20 +69,20 @@ const Section4 = () => {
             <div className="card-face">
               <Image alt="" src={Img4} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Olajumoke Akanmu</span>
-                <span className="card-desc">FINANCIAL SECRETARY</span>
+                <span className="card-title">AKINPELU PEACE</span>
+                <span className="card-desc">ASSIST. GENERAL SECRETARY</span>
               </div>
             </div>
           </div>
         </div>
-        {/* CARD 5 */}
+        {/* card 5 */}
         <div className="card">
           <div className="card-inner">
             <div className="card-face">
               <Image alt="" src={Img5} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Tella Titobiloluwa</span>
-                <span className="card-desc">WELFARE SECRETARY</span>
+                <span className="card-title">OLAPADE BURHANAT</span>
+                <span className="card-desc">FINANCIAL SECRETARY</span>
               </div>
             </div>
           </div>
@@ -92,20 +93,20 @@ const Section4 = () => {
             <div className="card-face">
               <Image alt="" src={Img6} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Uthman Olayinka</span>
-                <span className="card-desc">SPORTS SECRETARY</span>
+                <span className="card-title">ADEDEJI MA’ARUFAT</span>
+                <span className="card-desc">WELFARE SECRETARY</span>
               </div>
             </div>
           </div>
         </div>
-        {/* card 7 */}
+        {/* CARD 7 */}
         <div className="card">
           <div className="card-inner">
             <div className="card-face">
               <Image alt="" src={Img7} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Dolapo Fasida</span>
-                <span className="card-desc">SOCIAL SECRETARY</span>
+                <span className="card-title">AMADI CHUKWUEBUKA</span>
+                <span className="card-desc">SPORTS SECRETARY</span>
               </div>
             </div>
           </div>
@@ -116,19 +117,31 @@ const Section4 = () => {
             <div className="card-face">
               <Image alt="" src={Img8} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Esther Shogunle</span>
-                <span className="card-desc">TREASURER</span>
+                <span className="card-title">UKPABI ONYEDIKACHI</span>
+                <span className="card-desc">SOCIAL SECRETARY</span>
               </div>
             </div>
           </div>
         </div>
-        {/* CARD 9 */}
+        {/* card 9 */}
         <div className="card">
           <div className="card-inner">
             <div className="card-face">
               <Image alt="" src={Img9} className="img-front" />
               <div className="m-4 items-center justify-center flex flex-col">
-                <span className="card-title">Fashina Juwon</span>
+                <span className="card-title">DARAMOLA OLUWADITEMI</span>
+                <span className="card-desc">TREASURER</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* CARD 10 */}
+        <div className="card">
+          <div className="card-inner">
+            <div className="card-face">
+              <Image alt="" src={Img10} className="img-front" />
+              <div className="m-4 items-center justify-center flex flex-col">
+                <span className="card-title">OLUWOLE JOY</span>
                 <span className="card-desc">PUBLIC RELATION OFFICER</span>
               </div>
             </div>
