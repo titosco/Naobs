f3b12b85edb68e6b6784f7a2d92912cc022d8910
titosco <titobiloluwatella@gmail.com>
2026-01-07 11:36:05 +0100

    some

--- src/pages/Section/ToolBox/SectionTool.jsx
+++ src/pages/Section/ToolBox/SectionTool.jsx
@@ -1,14 +1,14 @@
-
-import Image from 'next/image'
-import React from 'react'
-import Img1 from '../../../images/undraw.png'
+import Image from "next/image";
+import React from "react";
+import Img1 from "../../../images/undraw.png";
 
 const SectionTool = () => {
   return (
-    <div className='flex flex-col items-center justify-center py-14 lg:py-28 w-full max-w-6xl mx-auto text-chart-1 text-4xl lg:text-8xl p-5 px-6 '>
-        <Image alt='' src={Img1} className='w-full'/>
+    // to be developed by the new administration
+    <div className="flex flex-col items-center justify-center py-14 lg:py-28 w-full max-w-6xl mx-auto text-chart-1 text-4xl lg:text-8xl p-5 px-6 ">
+      <Image alt="" src={Img1} className="w-full" />
     </div>
-  )
-}
+  );
+};
 
-export default SectionTool
+export default SectionTool;
